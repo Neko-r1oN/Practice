f7b1310bbee0b858666cc7db3aae4a1d5211c593
S-JOBI23-054\student <231076@st.yoshida.-g.ac.jp>
2024-08-30 09:32:22 +0900

    README用の画像追加

--- 0830 Console/obj/Debug/net6.0/0830 Console.AssemblyInfo.cs
+++ 0830 Console/obj/Debug/net6.0/0830 Console.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("0830 Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d96af53ab91462fc12c76e8012c2a27669ef3b6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc2e046a226a0e0de05c8d63080d0913c6c79230")]
 [assembly: System.Reflection.AssemblyProductAttribute("0830 Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("0830 Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
